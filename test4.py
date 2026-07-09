b7610c8abd526c7fe71e3ac07402b7f126f912c9
pscp-69070009 <69070009@kmitl.ac.th>
2026-07-09 21:29:10 +0700

    Create test4.py

---
+++ test4.py
@@ -0,0 +1,9 @@
+name = input()
+surname = input()
+age = input()
+
+if len(name) >= 5 and len(surname) >= 5:
+    password = name[:2] + surname[-1] + age[-1]
+else:
+    password = name[0] + age + surname[-1]
+print(password)
