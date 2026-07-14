45bdb85ddee69dd753b96426ea043b511f51ee9c
pscp-69070009 <69070009@kmitl.ac.th>
2026-07-14 21:42:23 +0700

    Create test6.py

---
+++ test6.py
@@ -0,0 +1,12 @@
+"""Program for reading characters and verifying codes."""
+char = input()
+digit = int(input())
+
+if char == "H" and digit == 4567:
+    print("safe unlocked")
+elif char == "H":
+    print("safe locked - change digit")
+elif digit == 4567:
+    print("safe locked - change char")
+else:
+    print("safe locked")
