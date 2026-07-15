6367b6292796d514e1988532fd932f41bb27cd84
pscp-69070009 <69070009@kmitl.ac.th>
2026-07-15 22:56:03 +0700

    Update 3011.py

--- 3011.py
+++ 3011.py
@@ -1,6 +1,6 @@
 """Colors."""
-color1 = input()
-color2 = input()
+color1 = input().capitalize()
+color2 = input().capitalize()
 
 if color1 == "Red" and color2 == "Yellow":
     print("Orange")
@@ -8,5 +8,11 @@ elif color1 == "Red" and color2 == "Blue":
     print("Purple")
 elif color1 == "Yellow" and color2 == "Blue":
     print("Green")
+elif color1 == "Red" and color2 == "Red":
+    print("Red")
+elif color1 == "Yellow" and color2 == "Yellow":
+    print("Yellow")
+elif color1 == "Blue" and color2 == "Blue":
+    print("Blue")
 else:
     print("Error")
