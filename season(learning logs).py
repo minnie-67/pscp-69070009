5dfc10fc26d2faf24c9ef3cf61c93c146cf4857a
pscp-69070009 <69070009@kmitl.ac.th>
2026-07-16 22:42:56 +0700

    Update season(learning logs).py

--- season(learning logs).py
+++ season(learning logs).py
@@ -1,23 +1,29 @@
-"""season."""
-month = int(input())
-day = int(input())
+"""season"""
 
-if month in [1, 2, 3]:
-    season = "winter"
-elif month in [4, 5, 6]:
-    season = "spring"
-elif month in [7, 8, 9]:
-    season = "summer"
-elif month in [10, 11, 12]:
-    season = "fall"
+def main():
+    """main"""
+    month = int(input())
+    day = int(input())
 
-if not month % 3 and day >= 21:
-    if season == "winter":
-        season = "spring"
-    elif season == "spring":
-        season = "summer"
-    elif season == "summer":
-        season = "fall"
-    elif season == "fall":
-        season = "winter"
-print(season)
+    if month in [1,2,3]:
+        Season = "winter"
+    elif month in [4,5,6]:
+        Season = "spring"
+    elif month in [7,8,9]:
+        Season = "summer"
+    elif month in [10,11,12]:
+        Season = "fall"
+
+    if not month % 3 and day >=21:
+        if Season == "winter":
+            Season = "spring"
+        elif Season == "spring":
+            Season = "summer"
+        elif Season == "summer":
+            Season = "fall"
+        elif Season == "fall":
+            Season = "winter"
+
+    print(Season)
+
+main()
