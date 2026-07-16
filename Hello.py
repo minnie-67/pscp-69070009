7acb41d6e9871a4cf4c8d08f211a61b8fa1be08e
pscp-69070009 <69070009@kmitl.ac.th>
2026-07-16 22:27:23 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 """Program to accept the user's first and last name."""
 
-name = input()
-surname = input()
+name = input("Enter your first name: ")
+surname = input("Enter your last name: ")
 
 print("Hello",name ,surname)
 print(name[:2] + surname[:2])
