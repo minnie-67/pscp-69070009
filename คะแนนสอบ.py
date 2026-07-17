0573c8f760e8839c8931f28cbe752b0f25f7e11c
pscp-69070009 <69070009@kmitl.ac.th>
2026-07-17 14:27:58 +0700

    Create คะแนนสอบ.py

---
+++ คะแนนสอบ.py
@@ -0,0 +1,15 @@
+"""คะแนนสอบ"""
+rabbit = int(input())
+max_score = 0
+top = 0
+
+for w in range(rabbit):
+    score = int(input())
+    if score > max_score:
+        max_score = score
+        top = 1
+    elif score == max_score:
+        top += 1
+
+    print(max_score)
+    print(top)
